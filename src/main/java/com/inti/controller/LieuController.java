e6381985f8f87d79554d97ffe23e402450ccdb58
lucieargaud <lucieargaud@gmail.com>
2023-03-15 16:23:49 +0100

    Controller Lieu Oeuvre Soliste

--- src/main/java/com/inti/controller/LieuController.java
+++ src/main/java/com/inti/controller/LieuController.java
@@ -1,8 +1,60 @@
 package com.inti.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.inti.model.Lieu;
+import com.inti.repository.ILieuRepository;
 
 @Controller
 public class LieuController {
+	@Autowired
+	ILieuRepository ilr;
 
+	@GetMapping("formLieu")
+	public String formLieu() {
+		return "formLieu";
+	}
+
+	@PostMapping("saveLieu")
+	public String saveLieu(@ModelAttribute("lieu") Lieu l) {
+		ilr.save(l);
+		return "redirect:/listeLieu";
+	}
+
+	@GetMapping("listeLieu")
+	public String listeLieu(Model m) {
+		m.addAttribute("listeLieu", ilr.findAll());
+		return "listeLieu";
+	}
+	@GetMapping("getLieu")
+	public String getLieu(@RequestParam("id") int id, Model m) {
+		m.addAttribute("lieu", ilr.findById(id).get());
+		return "getLieu";
+	}
+	@GetMapping("deleteLieu")
+	public String deleteLieu(@RequestParam("id") int id) {
+		ilr.deleteById(id);
+		return "redirect:/listeLieu";
+	}
+	
+	@GetMapping("modifierLieu")
+	public String modifierLieu(@RequestParam("id") int id, Model m)
+	{
+		m.addAttribute("lieu", ilr.findById(id).get());
+		return "modifierLieu";
+	}
+	
+	@PostMapping("updateLieu") // Avec une requête Post donc bien =/= du @Get"modifLieu"
+	public String updateLieu(@ModelAttribute("lieu") Lieu l)
+	{
+		ilr.save(l); // = saveOrUpdate
+		return "redirect:/listeLieu";
+	}
+	
 }
