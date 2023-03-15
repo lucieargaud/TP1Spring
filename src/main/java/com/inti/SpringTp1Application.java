ff7309e6a5a9f36f7a3dc9c6c0afc8d65023a49e
Jerem <Jerem@DESKTOP-8SBULL7>
2023-03-15 14:06:31 +0100

    create

---
+++ src/main/java/com/inti/SpringTp1Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTp1Application.class, args);
+	}
+
+}
