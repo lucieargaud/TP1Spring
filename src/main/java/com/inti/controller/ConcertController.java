1ec1119c8be2ccb1cc797120c66ff45ae02850e9
lucieargaud <lucieargaud@gmail.com>
2023-03-16 10:52:27 +0100

    Nom Fichier Concert + gitignore

--- src/main/java/com/inti/controller/ConcertController.java
+++ src/main/java/com/inti/controller/ConcertController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.inti.model.Concert;
 import com.inti.repository.IConcertRepository;
+import com.inti.repository.ILieuRepository;
 
 
 @Controller
@@ -53,21 +54,21 @@ public class ConcertController {
 	}
   	@GetMapping("deleteConcert")
 	public String deleteConcert(@RequestParam("num") int num) {
-		icor.deleteById(num);
+		icr.deleteById(num);
 		return "redirect:/listeConcert";
 	}
 	
 	@GetMapping("modifierConcert")
 	public String modifierConcert(@RequestParam("num") int num, Model m)
 	{
-		m.addAttribute("concert", icor.findById(num).get());
+		m.addAttribute("concert", icr.findById(num).get());
 		return "modifierConcert";
 	}
 	
 	@PostMapping("updateConcert") // Avec une requête Post donc bien =/= du @Get"modifconcert"
 	public String updateConcert(@ModelAttribute("concert") Concert c)
 	{
-		icor.save(c); // = saveOrUpdate
+		icr.save(c); // = saveOrUpdate
 		return "redirect:/listeConcert";
 	}
 	
